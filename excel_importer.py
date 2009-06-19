0ffee1e0a69e756bbbc1f8220c9b545ec327a3aa
Grant Paton-Simpson <g@ubuntu>
2009-06-20 01:21:30 +1200

    Importing from EXCEL works with or without a header row.  Added progress bar when importing.

---
+++ excel_importer.py
@@ -0,0 +1,110 @@
+import wx
+
+import dbe_plugins.dbe_sqlite as dbe_sqlite
+import excel_reader
+import importer
+import util
+
+ROWS_TO_SAMPLE = 500 # fast enough to sample quite a few
+
+
+class FileImporter(object):
+    """
+    Import excel file into default SOFA SQLite database.
+    Needs to identify data types to ensure only consistent data in a field.
+    Adds unique index id so can identify unique records with certainty.
+    """
+    
+    def __init__(self, file_path, tbl_name):                    
+        self.file_path = file_path
+        self.tbl_name = tbl_name
+        self.has_header = True
+        
+    def GetParams(self):
+        """
+        Get any user choices required.
+        """
+        retCode = wx.MessageBox("Does the spreadsheet have a header row?", 
+							    "HEADER ROW?", 
+							    wx.YES_NO | wx.ICON_INFORMATION)
+        self.has_header = (retCode == wx.YES)
+        return True
+    
+    def AssessDataSample(self, wksheet, fld_names, progBackup, gauge_chunk):
+        """
+        Assess data sample to identify field types based on values in fields.
+        If a field has mixed data types will define as string.
+        Returns fld_types, sample_data.
+        fld_types - dict with field names as keys and field types as values.
+        sample_data - list of dicts containing the first rows of data 
+            (no point reading them all again during subsequent steps).   
+        Sample first N rows (at most) to establish field types.   
+        """
+        debug = False
+        bolhas_rows = False
+        sample_data = []
+        for i, row in enumerate(wksheet):
+            if debug: print row
+            # if has_header, starts at 1st data row
+            bolhas_rows = True
+            # process row
+            sample_data.append(row)
+            gauge_val = i*gauge_chunk
+            progBackup.SetValue(gauge_val)
+            if i == (ROWS_TO_SAMPLE - 1):
+                break
+        fld_types = []
+        for fld_name in fld_names:
+            fld_type = importer.AssessSampleFld(sample_data, fld_name)
+            fld_types.append(fld_type)
+        fld_types = dict(zip(fld_names, fld_types))
+        if not bolhas_rows:
+            raise Exception, "No data to import"
+        return fld_types, sample_data
+    
+    def ImportContent(self, progBackup):
+        """
+        Get field types dict.  Use it to test each and every item before they 
+            are added to database (after adding the records already tested).
+        Add to disposable table first and if completely successful, rename
+            table to final name.
+        """
+        debug = False
+        try:
+            wkbook = excel_reader.Workbook(self.file_path)
+            sheets = wkbook.GetSheets()
+            if debug: print sheets
+            wksheet = wkbook.GetSheet(name=sheets[0], 
+                                      has_header=self.has_header)
+            n_rows = wksheet.GetRowsN()
+            # get field names
+            fld_names = wksheet.GetFldNames()
+            if debug: print fld_names
+            if self.has_header:
+                for row in wksheet: # prepare sheet to start after first row
+                    break
+        except Exception, e:
+            raise Exception, "Unable to read spreadsheet. " + \
+                "Orig error: %s" % e
+        conn, cur, _, _, _, _, _ = importer.GetDefaultDbDets()
+        sample_n = ROWS_TO_SAMPLE if ROWS_TO_SAMPLE <= n_rows else n_rows
+        gauge_chunk = importer.getGaugeChunkSize(n_rows, sample_n)
+        if debug: 
+            print "gauge_chunk: %s" % gauge_chunk
+            print "About to assess data sample"
+        fld_types, sample_data = self.AssessDataSample(wksheet, fld_names,
+                                                       progBackup, gauge_chunk)
+        if debug:
+            print "Just finished assessing data sample"
+            print fld_types
+            print sample_data
+        # NB wksheet will be at position ready to access records after sample
+        importer.AddToTmpTable(conn, cur, self.file_path, self.tbl_name, 
+                               fld_names, fld_types, sample_data, sample_n,
+                               remaining_data=wksheet, progBackup=progBackup,
+                               gauge_chunk=gauge_chunk)
+        importer.TmpToNamedTbl(conn, cur, self.tbl_name, self.file_path)
+        cur.close()
+        conn.commit()
+        conn.close()
+        progBackup.SetValue(0)
---
+++ excel_reader.py
@@ -0,0 +1,171 @@
+import win32com.client
+
+# http://www.w3schools.com/ADO/met_rs_open.asp
+adOpenForwardOnly = 0
+adOpenStatic = 3
+adLockReadOnly = 1
+adSchemaTables = 20
+ROW_BUFFER = 256 # for iteration. -1 buffers the entire worksheet
+
+def get_numbered_flds(n_flds):
+    return ["Fld_%s" % (x+1,) for x in range(n_flds)]
+
+def PlainProcess(val):
+    """
+    If value an ordinary string, return it stripped of extraneous whitespace.
+    If was all whitespace, return None.
+    If not a string, return as was. 
+    """
+    if isinstance(val, basestring):
+        val = val.strip()
+        if val == "":
+            return None
+    return val
+
+def EncodeProcess(val, encoding):
+    """
+    If unicode, encode if possible.
+    """
+    if isinstance(val, unicode):
+        val = val.strip()
+        if val == "":
+            return None
+        else:
+            return val.encode(encoding)
+    else:
+        return PlainProcess(val)
+       
+
+class Workbook(object):
+
+    def __init__(self, filename):
+        self.conn = win32com.client.Dispatch('ADODB.Connection')
+        self.conn.Open('PROVIDER=Microsoft.Jet.OLEDB.4.0;'+
+            'DATA SOURCE=%s;Extended Properties="Excel 8.0;HDR=1;IMEX=1"' % \
+            filename)
+
+    def GetSheets(self):
+        "Get list of worksheet names"
+        sheets = []
+        rs = self.conn.OpenSchema(adSchemaTables)
+        while not rs.EOF:
+            sheets.append(rs.Fields[2].Value)
+            rs.MoveNext()
+        rs.Close()
+        rs = None
+        del rs
+        return sheets
+
+    def GetSheet(self, name, has_header=True, encoding=None):
+        """
+        Get named worksheet.
+        has_header - has headings in first row?
+        encoding - character encoding used to encode the unicode strings 
+            returned by Excel, so we can use ordinary Python strings.
+        """
+        return Worksheet(self, name, has_header, encoding)
+			
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        "Closes Excel Workbook. Automatically called when Workbook deleted."
+        try:
+            self.conn.Close()
+            del self.conn
+        except Exception:
+            pass
+
+
+class Worksheet(object):
+    """
+    A worksheet which can be iterated through row by row.
+    has_header - first row contains column headings?
+    """
+    def __init__(self, doc, name, has_header, encoding):
+        self.doc = doc
+        self.name = name
+        self.has_header = has_header
+        self.encoding = encoding
+
+    def GetRowsN(self):
+        """
+        Need a static cursor to do a record count 
+        http://www.w3schools.com/ado/prop_rs_recordcount.asp
+        """
+        rs = win32com.client.Dispatch('ADODB.Recordset')		
+        rs.Open(u"SELECT * FROM [%s]" % self.name, self.doc.conn, adOpenStatic,
+                adLockReadOnly)
+        rows_n = rs.RecordCount
+        rs.Close()
+        rs = None
+        del rs
+        return rows_n
+
+    def GetFldNames(self):
+        """
+        Returns a list of field names for the sheet.
+        If a header, use those, otherwise follow convention Fld_1, ...
+        """
+        rs = win32com.client.Dispatch('ADODB.Recordset')		
+        try:
+            rs.Open(u'SELECT * FROM [%s]' % self.name, self.doc.conn,
+                adOpenForwardOnly, adLockReadOnly)
+        except Exception, e:
+            if e[2][2] == 'Too many fields defined.':
+                raise Exception, \
+                    "Delete blank columns at end of worksheet to keep " + \
+                    "within JET 255 column limit.  " + \
+                    "See http://support.microsoft.com/kb/198504/EN-US/"
+            else:
+                raise
+        try:
+            if self.has_header:
+                if self.encoding:
+                    fld_names = [EncodeProcess(fld.Name, self.encoding) \
+								 for fld in rs.Fields]
+                else:
+                	fld_names = [PlainProcess(fld.Name) for fld in rs.Fields]
+            else:
+                fld_names = get_numbered_flds(n_flds=len(rs.Fields))
+            return fld_names
+        finally:
+            rs.Close()
+            del rs
+
+    def __iter__(self):
+        "Returns paged iterator."
+        return self.BufferedRows()
+
+    def BufferedRows(self):
+        """
+        Returns an iterator on the rows in the worksheet. Rows are returned as
+            dicts with field names as keys.
+        """
+        rs = win32com.client.Dispatch('ADODB.Recordset')
+        rs.Open(u'SELECT * FROM [%s]' % self.name, self.doc.conn,
+                adOpenForwardOnly, adLockReadOnly)
+        try:
+            flds = self.GetFldNames()
+            rows_left = True
+            while True:
+                rows = zip(*rs.GetRows(ROW_BUFFER))				
+                if rs.EOF:
+                    rs.Close()
+                    rs = None
+                    rows_left = False
+                for row in rows:
+                	if self.encoding:
+                		vals = [EncodeProcess(x, self.encoding) for \
+								x in row]
+                		yield dict(zip(flds, vals))
+                	else:
+                		vals = [PlainProcess(x) for x in row]
+                		yield dict(zip(flds, vals))
+                if not rows_left:
+                	break
+        except Exception:
+            if rs is not None:
+                rs.Close()
+                del rs
+            raise
