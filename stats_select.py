4becbf01d006e579022d714a0c811a3e9e01f635
Grant Paton-Simpson <g@ubuntu>
2009-06-26 11:41:19 +1200

    Modified gateway dialog for statistical tests so more clear about choice between direct selection or assisted selection of test.  Initial wiring of buttons so enabled and disabled appropriately.

--- stats_select.py
+++ stats_select.py
@@ -17,7 +17,7 @@ TEST_PEARSONS = "Pearson's Correlation"
 STATS_TESTS = [TEST_TTEST, TEST_ANOVA, TEST_WILCOXON, TEST_MANN_WHITNEY,
                TEST_KRUSKAL_WALLIS, TEST_CHI_SQUARE, TEST_SPEARMANS,
                TEST_PEARSONS]
-MAIN_LEFT = 30
+MAIN_LEFT = 45
 
 
 class StatsSelectDlg(wx.Dialog):
@@ -41,68 +41,90 @@ class StatsSelectDlg(wx.Dialog):
                            wx.BITMAP_TYPE_XPM)
         self.SetIcons(ib)
         # background image
-        bk = "stats_select_win" if inwin else "stats_select"
         img_stats_select = wx.Image(os.path.join(SCRIPT_PATH, 
                                                  "images", 
-                                                 "%s.xpm" % bk ), 
+                                                 "stats_select.xpm"), 
                            wx.BITMAP_TYPE_XPM)
         self.bmp_stats_select = wx.BitmapFromImage(img_stats_select)
-        # with or without help
-        radDirect = wx.RadioButton(self.panel, -1, pos=(MAIN_LEFT, 55), 
-                                   style=wx.RB_SINGLE)
-        radDirect.Bind(wx.EVT_RADIOBUTTON, self.OnRadioDirectButton)
-        radAssisted = wx.RadioButton(self.panel, -1, pos=(MAIN_LEFT, 85), 
-                                     style=wx.RB_SINGLE)
-        radAssisted.Bind(wx.EVT_RADIOBUTTON, self.OnRadioAssistedButton)
-        # questions
+        # direct or assisted
+        self.radDirect = wx.RadioButton(self.panel, -1, 
+                            pos=(MAIN_LEFT-25, 55), 
+                            style=wx.RB_GROUP) # groups all till next RB_GROUP
+        self.radDirect.Bind(wx.EVT_RADIOBUTTON, self.OnRadioDirectButton)
+        self.radAssisted = wx.RadioButton(self.panel, -1, 
+                                          pos=(MAIN_LEFT - 25, 95))
+        self.radAssisted.Bind(wx.EVT_RADIOBUTTON, self.OnRadioAssistedButton)
+        # main assisted options
         self.radDifferences = wx.RadioButton(self.panel, -1, 
-                                             pos=(MAIN_LEFT, 115), 
-                                             style=wx.RB_SINGLE)
+                                             pos=(MAIN_LEFT, 135), 
+                                             style=wx.RB_GROUP)
+        self.radDifferences.Bind(wx.EVT_RADIOBUTTON, self.OnRadioDiffButton)
         self.radDifferences.Enable(False)
-        self.radRelationships = wx.RadioButton(self.panel, -1, 
-                                               pos=(MAIN_LEFT, 328), 
-                                               style=wx.RB_SINGLE)
+        self.radRelationships = wx.RadioButton(self.panel, -1,
+                                               pos=(MAIN_LEFT, 300))
+        self.radRelationships.Bind(wx.EVT_RADIOBUTTON, self.OnRadioRelButton)
         self.radRelationships.Enable(False)
-        # choices
-        DIFF_LN_1 = 180
-        n_group_choices = ["2 groups", "3 or more"]
-        if not inwin:
-            n_group_choices = ["%s " % x for x in n_group_choices]
-        self.radboxNGroups = wx.RadioBox(self.panel, -1, "No. of groups",
-                                         choices=n_group_choices,
+        # choices (NB can't use RadioBoxes and wallpaper in Windows)
+        # choices line 1
+        DIFF_LN_1 = 190
+        self.rad2Groups = wx.RadioButton(self.panel, -1, "2 groups", 
+                                         style=wx.RB_GROUP,
                                          pos=(MAIN_LEFT + 25, DIFF_LN_1))
-        self.radboxNGroups.Enable(False)
-        DIFF_LN_2 = 250
-        if inwin:
-            DIFF_LN_2 += 10
-        parametric_choices = ["Normal", "Not"]
-        if not inwin:
-            parametric_choices = ["%s " % x for x in parametric_choices]
-        self.radboxParametric1 = wx.RadioBox(self.panel, -1, "Parametric",
-                                             choices=parametric_choices,
-                                             pos=(MAIN_LEFT + 25, DIFF_LN_2))
-        self.radboxParametric1.Enable(False)
-        BUTTON_DROP = 10
-        if inwin:
-            BUTTON_DROP += 10
-        self.btnParametric1 = wx.Button(self.panel, -1, "Help me choose",
-                                        pos=(MAIN_LEFT + 165, 
-                                             DIFF_LN_2 + BUTTON_DROP)) 
-        self.btnParametric1.Enable(False)
-        DIFF_LN_3 = 410
-        self.radboxNonNumeric = wx.RadioBox(self.panel, -1, 
-                                            "Type of Data",
-                                            choices=["Categories", "Numbers"],
-                                            pos=(MAIN_LEFT + 25, DIFF_LN_3))
-        self.radboxNonNumeric.Enable(False)
-        self.radboxParametric2 = wx.RadioBox(self.panel, -1, "Parametric",
-                                             choices=parametric_choices,
-                                             pos=(MAIN_LEFT + 225, DIFF_LN_3))
-        self.radboxParametric2.Enable(False)
-        self.btnParametric2 = wx.Button(self.panel, -1, "Help me choose",
-                                       pos=(MAIN_LEFT + 365, 
-                                            DIFF_LN_3 + BUTTON_DROP))
-        self.btnParametric2.Enable(False)
+        self.rad3Groups = wx.RadioButton(self.panel, -1, "3 or more",
+                                         pos=(MAIN_LEFT + 125, DIFF_LN_1))
+        self.rad2Groups.Enable(False)
+        self.rad3Groups.Enable(False)
+        self.btnGroupsHelp = wx.Button(self.panel, -1, "Help", 
+                                       pos=(MAIN_LEFT + 255, DIFF_LN_1))
+        self.btnGroupsHelp.Enable(False)
+        # divider
+        wx.StaticLine(self.panel, pos=(MAIN_LEFT + 25, DIFF_LN_1 + 35),
+                      size=(190, 1))   
+        # choices line 2
+        DIFF_LN_2 = 240
+        lbl_normal = "Normal"
+        lbl_not_normal = "Not normal"
+        self.radNormal1 = wx.RadioButton(self.panel, -1, lbl_normal, 
+                                         style=wx.RB_GROUP,
+                                         pos=(MAIN_LEFT + 25, DIFF_LN_2))
+        self.radNotNormal1 = wx.RadioButton(self.panel, -1, lbl_not_normal,
+                                            pos=(MAIN_LEFT + 125, DIFF_LN_2))
+        self.radNormal1.Enable(False)
+        self.radNotNormal1.Enable(False)
+        self.btnNormalHelp1 = wx.Button(self.panel, -1, "Help",
+                                        pos=(MAIN_LEFT + 255, 
+                                             DIFF_LN_2)) 
+        self.btnNormalHelp1.Enable(False)
+        # choices line 3
+        DIFF_LN_3 = 360
+        self.radCats = wx.RadioButton(self.panel, -1, "Categories", 
+                                      style=wx.RB_GROUP,
+                                      pos=(MAIN_LEFT + 25, DIFF_LN_3))
+        self.radNums = wx.RadioButton(self.panel, -1, "Numbers",
+                                      pos=(MAIN_LEFT + 125, DIFF_LN_3))
+        self.radCats.Enable(False)
+        self.radNums.Enable(False)
+        self.btnTypeHelp = wx.Button(self.panel, -1, "Help", 
+                                     pos=(MAIN_LEFT + 255, DIFF_LN_3))
+        self.btnTypeHelp.Enable(False)
+        # divider
+        wx.StaticLine(self.panel, pos=(MAIN_LEFT + 25, DIFF_LN_3 + 35),
+                      size=(190, 1))   
+        # choices line 4
+        DIFF_LN_4 = 410
+        self.radNormal2 = wx.RadioButton(self.panel, -1, lbl_normal, 
+                                         style=wx.RB_GROUP,
+                                         pos=(MAIN_LEFT + 25, DIFF_LN_4))
+        self.radNotNormal2 = wx.RadioButton(self.panel, -1, lbl_not_normal,
+                                            pos=(MAIN_LEFT + 125, DIFF_LN_4))
+        self.radNormal2.Enable(False)
+        self.radNotNormal2.Enable(False)
+        self.btnNormalHelp2 = wx.Button(self.panel, -1, "Help",
+                                        pos=(MAIN_LEFT + 255, 
+                                             DIFF_LN_4)) 
+        self.btnNormalHelp2.Enable(False)
+        
+
         # listbox of tests
         LST_LEFT = 565
         LST_WIDTH = 200
@@ -125,8 +147,9 @@ class StatsSelectDlg(wx.Dialog):
             self.lstTests.SetItemImage(idx, 0, 0)
         self.lstTests.Select(0)
         # run test button
-        self.btnRun = wx.Button(self.panel, -1, "RUN TEST",
-                                pos=(675, LST_TOP + LST_HEIGHT + 20))
+        CONFIG_LEFT = 605 if not inwin else 615
+        self.btnConfig = wx.Button(self.panel, -1, "CONFIGURE TEST >>>",
+                                pos=(CONFIG_LEFT, LST_TOP + LST_HEIGHT + 20))
         # close button
         self.btnClose = wx.Button(self.panel, wx.ID_CLOSE,
                                   pos=(675, 500))
@@ -144,32 +167,83 @@ class StatsSelectDlg(wx.Dialog):
         panel_dc.SetTextForeground(TEXT_BROWN)
         panel_dc.SetFont(wx.Font(13, wx.SWISS, wx.NORMAL, wx.BOLD))
         panel_dc.DrawLabel("SELECT A STATISTICAL TEST HERE", 
-           wx.Rect(MAIN_LEFT + 25, 50, 100, 100))
+           wx.Rect(MAIN_LEFT, 50, 100, 100))
         panel_dc.DrawLabel("OR GET HELP CHOOSING BELOW", 
-           wx.Rect(MAIN_LEFT + 25, 85, 100, 100))
+           wx.Rect(MAIN_LEFT, 95, 100, 100))
         panel_dc.SetFont(wx.Font(11, wx.SWISS, wx.NORMAL, wx.BOLD))
         panel_dc.DrawLabel("Tests that show if there is a difference", 
-           wx.Rect(MAIN_LEFT + 25, 115, 100, 100))
+           wx.Rect(MAIN_LEFT + 25, 135, 100, 100))
         panel_dc.SetFont(wx.Font(11, wx.SWISS, wx.NORMAL, wx.NORMAL))
         panel_dc.DrawLabel("E.g. Do females have a larger vocabulary than " + \
                            "males?", 
-           wx.Rect(MAIN_LEFT, 145, 100, 100))
+           wx.Rect(MAIN_LEFT + 25, 160, 100, 100))
         panel_dc.SetFont(wx.Font(11, wx.SWISS, wx.NORMAL, wx.BOLD))
         panel_dc.DrawLabel("Tests that show if there is a relationship", 
-           wx.Rect(MAIN_LEFT + 25, 328, 100, 100))
+           wx.Rect(MAIN_LEFT + 25, 300, 100, 100))
         panel_dc.SetFont(wx.Font(11, wx.SWISS, wx.NORMAL, wx.NORMAL))
         panel_dc.DrawLabel("E.g. Does wealth increase with age?", 
-           wx.Rect(MAIN_LEFT, 360, 100, 100))
+           wx.Rect(MAIN_LEFT + 25, 327, 100, 100))
         event.Skip()
-    
-    def OnRadioDirectButton(self, event):
+        
+    def OnRadioDirectButton(self, event):        
+        self.radDifferences.SetValue(True)
         self.radDifferences.Enable(False)
+        self.DiffSetup(enable=False)
         self.radRelationships.Enable(False)
+        self.RelSetup(enable=False)
         
     def OnRadioAssistedButton(self, event):
         self.radDifferences.Enable(True)
-        self.radRelationships.Enable(True)        
+        self.DiffSetup(enable=True)
+        self.radRelationships.Enable(True)
+        self.RelSetup(enable=False)
     
+    def OnRadioDiffButton(self, event):
+        self.DiffSetup(enable=True)
+        self.RelSetup(enable=False)
+    
+    def OnRadioRelButton(self, event):
+        self.RelSetup(enable=True)
+        self.DiffSetup(enable=False)
+    
+    def DiffSetup(self, enable=True):
+        "Enable options under Differences section"
+        if not enable:
+            # set left first
+            try:
+                self.rad2Groups.SetValue(True)
+            except:
+                pass
+            try:
+                self.radNormal1.SetValue(True)
+            except:
+                pass
+        self.rad2Groups.Enable(enable)
+        self.rad3Groups.Enable(enable)
+        self.btnGroupsHelp.Enable(enable)
+        self.radNormal1.Enable(enable)
+        self.radNotNormal1.Enable(enable)
+        self.btnNormalHelp1.Enable(enable)
+
+    def RelSetup(self, enable=True):
+        "Enable options under Relationships section"
+        if not enable:
+            # set left first
+            try:
+                self.radCats.SetValue(True)
+            except:
+                pass
+            try:
+                self.radNormal2.SetValue(True)
+            except:
+                pass
+        self.radCats.Enable(enable)
+        self.radNums.Enable(enable)
+        self.btnTypeHelp.Enable(enable)
+        self.radNormal2.Enable(enable)
+        self.radNotNormal2.Enable(enable)
+        self.btnNormalHelp2.Enable(enable)
+        
     def OnCloseClick(self, event):
         self.Destroy()
         
