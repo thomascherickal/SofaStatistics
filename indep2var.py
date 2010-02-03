f125d2e5749356bbc4c4bb7e5526ee2ce0505908
Grant Paton-Simpson <g@grant>
2010-02-03 21:50:39 +1300

    Fixed bug exporting script to saved scripts - arguments in wrong order.

--- indep2var.py
+++ indep2var.py
@@ -491,7 +491,7 @@ class DlgIndep2VarConfig(wx.Dialog, config_dlg.ConfigDlg):
             add_to_report = self.chkAddToReport.IsChecked()
             css_fils, css_idx = output.get_css_dets(self.fil_report, 
                                                     self.fil_css)
-            script = self.get_script(css_idx, self.fil_report, add_to_report)
+            script = self.get_script(css_idx, add_to_report, self.fil_report)
             output.export_script(script, self.fil_script, 
                                  self.fil_report, css_fils, self.con_dets, 
                                  self.dbe, self.db, self.tbl, self.default_dbs, 
